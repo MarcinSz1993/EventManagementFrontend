47e2fd27fedbf5756e9c2b5bb46b3c1698ce4986
MarcinSz1993 <marcinsz1993@hotmail.com>
2025-02-14 10:14:19 +0100

    Build project for production.

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,4 +1,3 @@
-/*
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
@@ -6,4 +5,3 @@ import { config } from './app/app.config.server';
 const bootstrap = () => bootstrapApplication(AppComponent, config);
 
 export default bootstrap;
-*/
