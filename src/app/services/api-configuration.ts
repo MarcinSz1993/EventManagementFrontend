21a43bebf05d6cdbd20ade3c491dc16643893c26
MarcinSz1993 <marcinsz1993@hotmail.com>
2025-02-16 14:15:16 +0100

    Style corrections.

--- src/app/services/api-configuration.ts
+++ src/app/services/api-configuration.ts
@@ -11,7 +11,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ApiConfiguration {
-  rootUrl: string = 'https://eventmanagement-rypa.onrender.com';
+  rootUrl: string = 'https://eventmanagement-rypa.onrender.com/';
 }
 
 /**
